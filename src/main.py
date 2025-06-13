a5dcdcf466206f77efb93fa5528df95b633cf02c
Johana500 <84102123+Johana500@users.noreply.github.com>
2025-06-13 17:42:26 -0300

    Update main.py
    coreccion codigo

--- src/main.py
+++ src/main.py
@@ -1,38 +1,35 @@
-# archivo: main.py
-from user import Usuario
-from auth import registrar_usuario, iniciar_sesion, buscar_usuario
-from menu import menu_usuario, menu_admin
-
-# Lista que simula nuestra "base de datos"
-usuarios = [
-    Usuario("admin", "admin123", "admin"),  # Usuario inicial con rol admin
-]
+# main.py
+from sistema_auth import SistemaAuth
+from menu_sistema import MenuSistema
 
 def mostrar_menu_principal():
-    print("\n SISTEMA DE USUARIOS - E-commerce de Tecnología")
+    print("\nSISTEMA DE USUARIOS - E-commerce de Tecnología")
     print("1. Registrar usuario")
     print("2. Iniciar sesión")
     print("3. Salir")
 
 def main():
+    auth = SistemaAuth()
+    menu = MenuSistema()
+
     while True:
         mostrar_menu_principal()
         opcion = input("Seleccione una opción: ")
 
         if opcion == "1":
-            registrar_usuario(usuarios)
+            auth.registrar_usuario()
         elif opcion == "2":
-            usuario = iniciar_sesion(usuarios)
+            usuario = auth.iniciar_sesion()
             if usuario:
                 if usuario.rol == "admin":
-                    menu_admin(usuarios)
+                    menu.mostrar_menu_admin(auth)
                 else:
-                    menu_usuario(usuario)
+                    menu.mostrar_menu_usuario(usuario)
         elif opcion == "3":
-            print(" ¡Hasta luego!")
+            print("¡Hasta luego!")
             break
         else:
-            print(" Opción inválida.")
+            print("Opción inválida.")
 
 if __name__ == "__main__":
     main()
