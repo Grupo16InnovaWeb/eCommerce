1ee066db013c21303ef82b53e3fc0849a0d0706d
FernandoAndreoli <81665327+FernandoAndreoli@users.noreply.github.com>
2025-06-13 21:24:54 -0300

    Update CRUD para Usuarios.sql
    doble guion para comentar

--- database/CRUD para Usuarios.sql
+++ database/CRUD para Usuarios.sql
@@ -1,9 +1,9 @@
-✅ CREATE (Insertar nuevo usuario)
+--✅ CREATE (Insertar nuevo usuario)
 
 INSERT INTO usuarios (nombre_usuario, contraseña, rol)
 VALUES ('juan23', 'clave123', 'usuario');
 
-✅ READ (Consultar usuarios)
+--✅ READ (Consultar usuarios)
 a. Todos los usuarios
 SELECT * FROM usuarios;
 
@@ -13,12 +13,12 @@ SELECT * FROM usuarios
 WHERE nombre_usuario = 'juan23';
 
 
-✅ UPDATE (Modificar contraseña o rol)
+--✅ UPDATE (Modificar contraseña o rol)
 UPDATE usuarios
 SET contraseña = 'nuevaClave456', rol = 'admin'
 WHERE nombre_usuario = 'juan23';
 
 
-✅ DELETE (Eliminar usuario)
+--✅ DELETE (Eliminar usuario)
 DELETE FROM usuarios
 WHERE nombre_usuario = 'juan23';
